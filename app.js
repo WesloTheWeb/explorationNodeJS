1b778da32e66ff0a448cc26f865b0ccf9bc4c0aa
Wesley <wesleyadwebster@gmail.com>
2023-02-17 04:24:59 -0600

    served fully functional HTML page, routes added and CSS

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const adminRoute = require('./routes/admin'); // order of imports does not matter
 const shopRoute = require('./routes/shop');
-
+const path = require('path')
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// allows static CSS file to be used.
+app.use(express.static(path.join(__dirname, 'public')));
 
 // order matters on middleware
 app.use('/admin', adminRoute);
@@ -13,7 +16,8 @@ app.use(shopRoute);
 
 // 404 page catch all
 app.use((req, res, next) => {
-    res.status(404).send(`<h1>Error 404: Page not Found.</h2>`);
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).send(`<h1>Error 404: Page not Found.</h2>`);
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-products', (req, res, next) => {
-    res.send('<form action="/admin/add-products" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 
+// /admin/add-products => POST
 router.post('/add-products', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
