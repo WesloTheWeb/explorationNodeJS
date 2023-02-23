5fca258e1c9c94d30d9c497d47c2fc9aadc26d49
Wesley <wesleyadwebster@gmail.com>
2023-02-22 18:25:31 -0600

    Added in more views and an admin controller. Form is about to change

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const productsControlller = require('../controllers/products');
-
+const adminController = require('../controllers/admin');
 const router = express.Router();
 
 // /admin/add-products => GET
-router.get('/add-products', productsControlller.getAddProducts);
+router.get('/add-product', productsControlller.getAddProducts);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-products => POST
 router.post('/add-products', productsControlller.postAddProducts);
