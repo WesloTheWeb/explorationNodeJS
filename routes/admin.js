41c12cbd998c750cd0c9c2d8370a4b3ad057f427
Wesley <wesleyadwebster@gmail.com>
2023-02-23 01:07:10 -0600

    Navigation styling and mobile added. Logic and routes updated

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 // /admin/add-products => GET
-router.get('/add-product', adminController.getAddProducts);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
 // /admin/add-products => POST
-router.post('/add-products', adminController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
