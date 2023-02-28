13094890d0d1fa3a05fefc58b4e555b922ac1fe6
Wesley <wesleyadwebster@gmail.com>
2023-02-28 01:24:48 -0600

    deletion progress

--- models/cart.js
+++ models/cart.js
@@ -45,4 +45,35 @@ module.exports = class Cart {
             });
         });
     };
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(cartPathwayFile, (err, fileContent) => {
+            if (err) {
+                return;
+            }
+            const updatedCart = { ...JSON.parse(fileContent) };
+            const product = updatedCart.products.find(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(
+                prod => prod.id !== id
+            );
+            updatedCart.totalPrice =
+                updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(cartPathwayFile, JSON.stringify(updatedCart), err => {
+                console.log(err);
+            });
+        });
+    };
+
+    static getCart(cb) {
+        fs.readFile(cartPathwayFile, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if (err) {
+                cb(null);
+            } else {
+                cb(cart);
+            }
+        });
+    };
 };
