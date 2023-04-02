cec7cbdeaa95775125023f7d5de7a306f6399b67
Wesley Webster <wesleyadwebster@gmail.com>
2023-04-02 03:28:18 -0500

    Feat/sequelize integration (#2)
    * Initial commit and set up of sequelize
    
    * Set up of sequelize data schema
    
    * Refactored postAddProduct function that now, with Sequelize, adds to our table from the web form
    
    * Refactored fetching data for product details and fixed image sizing
    
    * Updating products work from the form to the sql database and fetches and reads from it as well
    
    * Relation made sequelize
    
    * Initialize with a dummy user. We can add, edit and delete from the admin view. SQL Relations of user and product created in SQL base dynamically.
    
    * small refactor on the getEditProducts. We chained methods to resolve promise
    
    * Created Cart and CartItem models
    
    * Set up to be fixed, adding to cart
    
    * Fixed the issue of adding to cart, added css. Added details button to index shop. Ready to go for C&B morning
    
    * added costs to the cart display
    
    * Users are now able to delete a cart item in the cart view. Relational link has been added in the postCartDeleteProduct.
    
    * Added relations table and models for Order and OrderItem
    
    * Created order route and postOrder method
    
    * Added in clearing the cart and viewing orders in the tab. Deleted checkout page

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const adminRoutes = require('./routes/admin'); // order of imports does not matter
 const shopRoute = require('./routes/shop');
-const errorController = require('./controllers/error')
-const path = require('path')
+const errorController = require('./controllers/error');
+const path = require('path');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-Item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
+const sequelize = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// order matters on middleware:
 // parses form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,11 +27,58 @@ app.use(express.json());
 // allows static CSS file to be used.
 app.use(express.static(path.join(__dirname, 'public')));
 
-// order matters on middleware
+// dummy data
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
+// ASSOCIATION - Sequelize:
+Product.belongsTo(User, { constraints: true, onDelee: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
 app.use('/admin', adminRoutes);
 app.use(shopRoute);
 
 // 404 page catch all
 app.use(errorController.get404);
 
-app.listen(3000);
+
+// old set up
+// sequelize.sync({ force: true })
+//     .then((result) => {
+//         // console.log(result)
+//         app.listen(3000);
+//     })
+//     .catch(err => console.log(err));
+
+// Dummy User set up:
+sequelize.sync()
+    // sequelize.sync({ force: true })
+    .then((result) => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Wes', email: 'deezenuts143@uwu.com' })
+        }
+        return user;
+    })
+    .then(user => {
+        return user.createCart();
+    })
+    .then(cart => {
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
