d2f2e682c3a9bac06b0afa6eef0910e409ec89a4
Wesley <wesleyadwebster@gmail.com>
2023-04-07 23:53:07 -0500

    displays item and quantity on carts page that is rendered.

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,19 +39,14 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  console.log('CART:', req.user.cart);
   req.user
     .getCart()
-    .then(cart => {
-      return cart.getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then((products) => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -64,6 +59,7 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
+      res.redirect('/cart');
     })
     .catch(err => console.log(err));
   // let fetchedCart;
