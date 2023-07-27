0abc0215ac1ad8e16c401a934cd41624721eb3e9
Wesley <wesleyadwebster@gmail.com>
2023-07-26 20:06:15 -0500

    users can logout. Encountered bug on orders

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,3 +19,21 @@ exports.postLogin = (req, res, next) => {
         })
         .catch(err => console.log(err));
 };
+
+exports.postLogin = (req, res, next) => {
+    User.findById("643359cbb3bfb460f2f91522")
+        .then(user => {
+            req.session.isLoggedIn = true;
+            // set the user here
+            req.session.user = user;
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,6 @@ router.get('/login', authController.getLogin);
 
 router.post('/login', authController.postLogin);
 
-// router.get('/login', (req, res, next) => {
-//     const isLoggedIn = req
-//       .get('Cookie')
-//       .split(';')[1]
-//       .trim()
-//       .split('=')[1] === 'true';
-// });
+router.post('/logout', authController.postLogout);
 
 module.exports = router;
