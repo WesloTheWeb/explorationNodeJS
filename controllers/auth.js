d01b548d5f1d7b6a9c4b90140fa05464b6095d7d
Wesley <wesleyadwebster@gmail.com>
2023-11-19 16:49:48 -0600

    Deleted duplicate function, getting back into this project and course with sessions and cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    console.log(req.session.isLoggedIn);
+    console.log('session', req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -16,17 +16,6 @@ exports.getSignup = (req, res, next) => {
       isAuthenticated: false
     });
   };  
-
-exports.postLogin = (req, res, next) => {
-    User.findById("643359cbb3bfb460f2f91522")
-        .then(user => {
-            req.session.isLoggedIn = true;
-            // set the user here
-            req.session.user = user;
-            res.redirect('/');
-        })
-        .catch(err => console.log(err));
-};
 
 exports.postSignup = (req, res, next) => {};
 
