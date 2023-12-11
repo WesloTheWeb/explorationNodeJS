79e03ae904b42f11758256c0b7b0bb29fd3ba957
Wesley Webster <wesleyadwebster@gmail.com>
2023-12-10 19:32:09 -0600

    Added in route protection for authenticated users, created is-Auth helper method

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,14 +51,33 @@ exports.postSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById("643359cbb3bfb460f2f91522")
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email })
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save((err) => {
-                console.log(err)
-                res.redirect('/');
-            });
+            if (!user) {
+                return res.redirect('/login')
+            };
+
+            bcrypt.compare(password, user.password)
+                .then((doMatch) => {
+                    if (doMatch) {
+                        console.log('password matched success')
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save((err) => {
+                            console.log(err)
+                            res.redirect('/');
+                        });
+                    };
+
+                    res.redirect('/login');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
         })
         .catch(err => console.log(err));
 };
