fdb1af8388b3fd0c92b06c6f4c7bb9a0d3d1d783
Wesley <wesleyadwebster@gmail.com>
2024-01-01 21:45:14 -0600

    start of error handling

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.use((req, res, next) => {
     next();
 });
 
-
 app.use(flash());
 
 app.use((req, res, next) => {
@@ -54,11 +53,17 @@ app.use((req, res, next) => {
     };
 
     User.findById(req.session.user._id)
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err => console.log(err));
+        .then(user => {
+            if (!user) {
+                return next();
+            };
+
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
 });
 
 app.use('/admin', adminRoutes);
