0d47ef411b7abf88ecc9bb6f905c4542e2f94a89
Wesley Webster <wesleyadwebster@gmail.com>
2024-01-13 00:58:01 -0600

    Feat/file uploads (#7)
    * Added Mullter package and work on file parsing
    
    * Added support for uploading images, fixed bugs of invalid loggedIn
    
    * product images now render
    
    * Reading a pdf file and invoice logic
    
    * pdf generation logic added to order invoices
    
    * Deletion logic works to delete the corresponding uploads on the file system
    
    * cleared unused code

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const fileHelper = require('../util/file');
 const { validationResult } = require('express-validator');
 const Product = require('../models/product');
 
@@ -17,8 +18,26 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const price = req.body.price;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const description = req.body.description;
+
+  if (!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: "Attached file is not a valid image format.",
+      validationErrors: [],
+      isAuthenticated: req.session.isLoggedIn
+    });
+  };
+
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -39,6 +58,8 @@ exports.postAddProduct = (req, res, next) => {
       isAuthenticated: req.session.isLoggedIn
     });
   };
+
+  const imageUrl = image.path;
 
   const product = new Product(
     {
@@ -114,7 +135,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
@@ -127,7 +148,6 @@ exports.postEditProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId
@@ -147,7 +167,12 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+
+      if (image) {
+        fileHelper.deleteFile(product.imageUrl);
+        product.imageUrl = image.path;
+      };
+
       return product.save().then((result) => {
         console.log('UPDATED PRODUCT');
         res.redirect('/admin/products');
@@ -179,7 +204,16 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({ _id: prodId, userId: req.user._id })
+
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error('Product not found'));
+      };
+
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id })
+    })
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
+const PDFDocument = require('pdfkit');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -107,6 +110,7 @@ exports.postOrder = (req, res, next) => {
   // take all cart item and move them into an order
   req.user
     .populate('cart.items.productId')
+    .execPopulate()
     .then((user) => {
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
@@ -153,44 +157,43 @@ exports.getOrders = (req, res, next) => {
     });
 };
 
-/* Note:
-The reason we are able to call req.user
-.<FUNCTION> is because in our app.js,
-we set our req.user
- = user, which is our User class model because
-findById returns 
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
 
+  Order.findById(orderId)
+    .then((order) => {
+      const invoiceName = 'invoice-' + orderId + '.pdf';
+      const invoicePath = path.join('data', 'invoices', invoiceName);
 
-req.user
- is being used as an instance of the User model which has been obtained from the database using the findById method. 
-The User model is defined in the ./models/user.js file.
+      const pdfDoc = new PDFDocument();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        'inline; filename="' + invoiceName + '"'
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res); // writes to client
 
-When the findById method returns a user object, it is attached to the req object by assigning it to the req.user
- property. 
-This is done using the middleware function:
+      // pdf styling
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true
+      });
+      pdfDoc.text("-----------------------");
+      let totalPrice = 0;
 
-app.use((req, res, next) => {
-    User.findById("643359cbb3bfb460f2f91522")
-        .then(user => {
-            req.user
- = user
-            next();
-        })
-        .catch(err => console.log(err));
-});
-
-After attaching the user object to req.user
-, it can be used in subsequent middleware functions and route handlers.
-
-In the postOrder function, req.user
-.populate('cart.items.productId') is used to load the productId 
-of each item in the cart. Then, req.user
- is used to get the name and userId of the user who is placing the order. 
-Finally, req.user
-.clearCart() is called to clear the cart of the user after the order has been placed.
-
-In the getOrders function, req.user
-.getOrders() is used to get the orders placed by the user. 
-Here, getOrders() is assumed to be a custom method defined on the User model, which retrieves the orders 
-related to the user.
-*/
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(`
+            ${prod.product.title} - ${prod.quantity} x $${prod.product.price}
+          `)
+      });
+      pdfDoc.text("-----------------------");
+      pdfDoc
+        .fontSize(20)
+        .text(`Total Price: $${totalPrice}`);
+      pdfDoc.end();
+    })
+    .catch((err) => next(err));
+};
